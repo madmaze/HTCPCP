986177dad932fe6cff29f16d7ff0e43ca6a1459a
James Eastwood <JDEastwood@gmail.com>
2011-08-03 01:41:38 -0400

    Completed the brew function!

--- pot.c
+++ pot.c
@@ -1,12 +1,13 @@
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 //for testing
 #include <stdio.h>
 
 
 #define BUFFSIZE 4096
-#define ERROR -1
+#define ERROR	0 
 #define TRUE	1
 #define FALSE 	0
 #define READY	1
@@ -20,7 +21,12 @@
 #define SECS_TO_COLD	60
 #define FREE_UNITS_FOR_ADDS	100
 
-char additions[] = "CREAM;HALF-AND-HALF;WHOLE-MILK;PART-SKIM;SKIM;NON-DAIRY;VANILLA;ALMOND;RASPBERRY;CHOCOLATE;WHISKY;RUM;KAHLUA;AQUAVIT";
+//Return codes
+#define C_POT_BUSY "HTCPCP/1.0 510 Pot busy \r\n "
+#define C_INVALID_ADDS "HTCPCP/1.0 406 Not Acceptable \r\nAdditions-List:CREAM;HALF-AND-HALF;WHOLE-MILK;PART-SKIM;SKIM;NON-DAIRY;VANILLA;ALMOND;RASPBERRY;CHOCOLATE;WHISKY;RUM;KAHLUA;AQUAVIT\r\n" 
+#define C_BREW_SUC "HTCPCP/1.0 200 OK \r\nContent-length: 0\r\nContent-type: message/coffeepot \r\nETA: 30\r\n" 
+
+char validAdditions[] = "CREAM;HALF-AND-HALF;WHOLE-MILK;PART-SKIM;SKIM;NON-DAIRY;VANILLA;ALMOND;RASPBERRY;CHOCOLATE;WHISKY;RUM;KAHLUA;AQUAVIT";
 
 typedef struct {
 	time_t finBrew;
@@ -31,6 +37,35 @@ typedef struct {
 	char waitingAdditions[20][255];
 } potStruct;
 
+const char *mystristr(const char *haystack, const char *needle)
+{
+	if ( !*needle )
+	{
+		return haystack;
+	}
+	for ( ; *haystack; ++haystack )
+	{
+		if ( toupper(*haystack) == toupper(*needle) )
+		{
+			/*
+			 *           * Matched starting char -- loop through remaining chars.
+			 *                     */
+			const char *h, *n;
+			for ( h = haystack, n = needle; *h && *n; ++h, ++n )
+			{
+				if ( toupper(*h) != toupper(*n) )
+				{
+					break;
+				}
+			}
+			if ( !*n ) /* matched all of 'needle' to null termination */
+			{
+				return haystack; /* return the start of the match */
+			}
+		}
+	}
+	return 0;
+}
 
 //constructor for our pot struct.
 void resetPot(potStruct * pot) {
@@ -60,8 +95,8 @@ int getState(potStruct * pot) {
 		if(difftime(curTime, pot->finBrew) >= SECS_TO_COLD) {
 			return(CUP_COLD);
 		}
-		else if (pot->startPour >= curTime) {
-			if((((int)difftime(pot->startPour, curTime))*pot->addUnitsPerSec) > FREE_UNITS_FOR_ADDS) {
+		else if (pot->startPour <= curTime && pot->startPour > 0) {
+			if((((int)difftime(curTime, pot->startPour))*pot->addUnitsPerSec) > FREE_UNITS_FOR_ADDS) {
 				return(CUP_OVERFLOW);
 			}
 			else if (pot->additionsAdded == TRUE) {
@@ -82,21 +117,94 @@ int getState(potStruct * pot) {
 
 }
 
+int validateAdds(char additions[][255]) {
+	char * tok;
+	int i;
+
+	for(i=0; i < 20; i++) {
+		tok=strtok(additions[i], ";");
+		if(tok == NULL) {
+			return(TRUE);
+		}
+		if(mystristr(validAdditions,tok) == NULL) {
+			return(FALSE);
+		}
+	}
+
+
+}
+
+int calcAddPerSec(char additions[][255]) {
+	char * tok;
+	int i;
+	int count=0;
+
+	for(i=0; i < 20; i++) {
+		tok=strtok(additions[i], ";");
+		tok=strtok(NULL,"");
+		if(tok == NULL)
+			return(count);
+		count+=atoi(tok);
+	}
+	return(count);
+
+}
+
+void brew(potStruct * pot, char additions[][255], char * buf) {
+	int potStatus=getState(pot);
+	int i;
+
+	if(potStatus != READY) {
+		strcpy(buf,C_POT_BUSY);	
+		return;
+	}
+
+	if(validateAdds(additions) == FALSE) {
+		strcpy(buf,C_INVALID_ADDS);
+		return;
+	}
+
+	strcpy(buf, C_BREW_SUC);
+	resetPot(pot);
+
+	pot->finBrew=time(NULL)+30;
+
+	for(i=0; i < 20; i++) {
+		strcpy(pot->waitingAdditions[i],additions[i]);
+	}
+
+	pot->addUnitsPerSec=calcAddPerSec(pot->waitingAdditions);
+
+
+
+	printf("Additions: %d \n", pot->addUnitsPerSec);
+
+}
+
 int main() {
 
 	potStruct pot;
 
 	int result;
+	char test[BUFFSIZE];
+	char twoTest[20][255];
 
-
+	strcpy(twoTest[0], "Milk;1");
+	strcpy(twoTest[1], "Rum;3");
+	
 	resetPot(&pot);
+	result=calcAddPerSec(twoTest);
+	brew(&pot, twoTest, test);
+	//resetPot(&pot);
 
-	pot.cupWaiting=TRUE;
-	pot.finBrew=time(NULL)-50;
-	pot.addUnitsPerSec=45;
-	pot.startPour=time(NULL);
-	result=getState(&pot);
+	/*	pot.cupWaiting=TRUE;
+		pot.finBrew=time(NULL)-50;
+		pot.addUnitsPerSec=1;
+		pot.startPour=time(NULL)-5;
+		pot.additionsAdded=TRUE;*/
+	//result=getState(&pot);
 
 	printf("Result: %d\n", result);
+	printf("Buf: %s\n", test);
 
 }
